82f5c15d62a5542a48b03953ea1449bdd45fca24
dlfud <99ysl@naver.com>
2022-08-11 16:27:16 +0900

    setting

---
+++ src/main/java/com/instaKG/InstaKgApplication.java
@@ -0,0 +1,13 @@
+package com.instaKG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstaKgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstaKgApplication.class, args);
+	}
+
+}
